2e5a75694fd658c48767809f1b3f106332d588d5
Corey Oordt <coreyoordt@gmail.com>
2012-08-20 14:32:32 -0500

    Update faq/urls/shallow.py
    Made the shallow urls follow the /faq/#topic and /faq/#question model

--- faq/urls/shallow.py
+++ faq/urls/shallow.py
@@ -14,7 +14,7 @@ from faq.views.shallow import topic_list, topic_detail, question_detail
 
 urlpatterns = patterns('',
     url(r'^$', topic_list, name='faq-topic-list'),
-    url(r'^(?P<slug>[-\w]+)/$', topic_detail, name='faq-topic-detail'),
-    url(r'^(?P<topic_slug>[-\w]+)/(?P<slug>[-\w]+)/$', question_detail,
+    url(r'^#(?P<slug>[-\w]+)$', topic_list, name='faq-topic-detail'),
+    url(r'^#(?P<slug>[-\w]+)$', topic_list,
         name='faq-question-detail'),
 )
